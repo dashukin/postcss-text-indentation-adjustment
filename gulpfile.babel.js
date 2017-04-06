4f98ec1465da68c5f1247cc7cd783cb66e13cf28
Vasili_Molakhau <vasili_molakhau@epam.com>
2017-04-06 18:08:50 +0300

    Create tasks

---
+++ gulpfile.babel.js
@@ -0,0 +1,89 @@
+/*
+* gulp tasks
+* */
+
+import gulp from 'gulp';
+import del from 'del';
+import gulpPostcss from 'gulp-postcss';
+import postcssScss from 'postcss-scss';
+import sass from 'gulp-sass';
+import textMetrics from './index';
+
+// init text metrics plugin
+const textMetricsPlugin = textMetrics({
+	textMetrics: {
+		'p1': [
+			{
+				atRule: null,
+				selector: '.only-parent-selector',
+				// line-height minus font-size minus any additional decreaseBy value
+				baseDelta: 4, // (line-height - font-size) / 2
+				decreaseBy: 1 // text metrics
+			},
+			{
+				atRule: {
+					name: 'media',
+					params: '(max-width: 1499px)'
+				},
+				selector: null,
+				// line-height minus font-size minus any additional decreaseBy value
+				baseDelta: 6, // (line-height - font-size) / 2
+				decreaseBy: 1 // text metrics
+			},
+			{
+				atRule: {
+					name: 'media',
+					params: '(max-width: 1499px)'
+				},
+				selector: '.parent-selector-with-atrule',
+				// line-height minus font-size minus any additional decreaseBy value
+				baseDelta: 8, // (line-height - font-size) / 2
+				decreaseBy: 1 // text metrics
+			}
+		]
+	}
+});
+
+const source = {
+	css: './example/src/css/**/*.css',
+	scss: './example/src/scss/**/*.scss'
+};
+
+const output = {
+	css: './example/dist/css',
+	scss: './example/dist/scss'
+}
+
+gulp.task('compile:css', () => {
+	del(`${output.css}/*`);
+	gulp.src(source.css)
+		.pipe(gulpPostcss([textMetricsPlugin]))
+		.pipe(gulp.dest(output.css));
+});
+
+gulp.task('compile:scss', () => {
+	del(`${output.scss}/*`);
+	gulp.src(source.scss)
+		.pipe(gulpPostcss([textMetricsPlugin]), {
+			syntax: postcssScss
+		})
+		.pipe(gulp.dest(output.scss));
+});
+
+gulp.task('compile:all', () => {
+	console.log('Not implemented');
+});
+
+gulp.task('watch:css', () => {
+	gulp.watch(source.css, ['compile:css']);
+});
+
+gulp.task('watch:scss', () => {
+	gulp.watch(source.scss, ['compile:scss']);
+});
+
+gulp.task('watch:all', () => {
+	console.log('Not implemented');
+});
+
+gulp.task('default', ['compile:css', 'watch:css', 'compile:scss', 'watch:scss']);
