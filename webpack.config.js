c37258d2a9dfdc42d93361774dd0af930d169291
dashukin <dashukin@gmail.com>
2017-10-11 11:11:55 +0300

    Update webpack config. Add unminified build version

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,13 @@ var webpack = require('webpack');
 var path = require('path');
 
 module.exports = {
-	entry: './src/index',
+	entry: {
+		'index': './src/index',
+		'index.min': './src/index'
+	},
 	output: {
 		path: path.resolve(__dirname, 'dist'),
-		filename: 'index.js',
+		filename: '[name].js',
 		libraryTarget: 'commonjs2',
 	},
 	module: {
@@ -21,6 +24,9 @@ module.exports = {
 				"NODE_ENV": JSON.stringify("production")
 			}
 		}),
-		new webpack.optimize.UglifyJsPlugin()
+		new webpack.optimize.UglifyJsPlugin({
+			include: /\.min\.js$/,
+			minimize: true
+		})
 	]
 };
